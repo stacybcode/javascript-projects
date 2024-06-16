f123ab76d7f24c6c4f155e08a8a4656f9da4e43a
Stacy Knapp <franksmom@MacBook-Air.localdomain>
2024-06-16 17:34:29 -0500

    Moon Project

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,30 @@
 // Declare and assign the variables below
+let shuttleName = "Determination";
+let shuttleMPH = 17500;
+let marsDistance = 225000000;
+let moonDistance = 384400;
+const milesPerKm = 0.621;
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof shuttleName);
+console.log(typeof shuttleMPH);
+console.log(typeof marsDistance);
+console.log(typeof moonDistance);
+console.log(typeof milesPerKm);
 
 // Calculate a space mission below
+let milesToMars = marsDistance * milesPerKm;
+let hoursToMars = milesToMars / shuttleMPH;
+let daysToMars = hoursToMars / 24;
 
 // Print the results of the space mission calculations below
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.");
 
 // Calculate a trip to the moon below
+let milesToMoon = moonDistance * milesPerKm;
+let hoursToMoon = milesToMoon / shuttleMPH;
+let daysToMoon = hoursToMoon / 24;
+
 
 // Print the results of the trip to the moon below
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
